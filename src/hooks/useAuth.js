8b97b406697799cabe931e367bc1143ffac391c6
dineshkarthickd <dineshkarthick1610@gmail.com>
2026-07-09 13:21:27 +0530

    fix: remove unused services and utility functions

--- src/hooks/useAuth.js
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react';
-import { AuthContext } from '../context/AuthContext';
-import { AdminContext } from '../context/AdminContext';
-
-export const useAuth = () => {
-  const authCtx = useContext(AuthContext);
-  const adminCtx = useContext(AdminContext);
-  return { ...authCtx, ...adminCtx };
-};
